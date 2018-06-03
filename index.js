b1cfa0a782768053c055761b92a6edf0fa2bea84
queenCee <aakarshitatiwari@gmail.com>
2018-06-03 16:06:15 +0530

    simple node example

---
+++ index.js
@@ -0,0 +1,21 @@
+var rect = {
+    perimeter: (x,y) => (2*(x+y)),
+    area: (x,y) => (x*y)
+};
+function solveRect(l,b){
+    console.log("solving for rectangle with legth = "+ l + " and breadth = "+ b);
+    if(l<= 0 || b<= 0){
+        console.log("invalid dimmentions");
+    }
+    else{
+        console.log(" perimeter = " + rect.perimeter(l,b) );
+        console.log("area = " + rect.area(l,b));
+    }
+
+}
+solveRect(2,4);
+solveRect(0,3);
+solveRect(-3,5);
+solveRect(10,3);
+
+ 
