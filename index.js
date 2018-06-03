841db81793050918ec2222d81ff0dff3401845c2
queenCee <aakarshitatiwari@gmail.com>
2018-06-03 19:41:36 +0530

    using callbacks and error handling

--- index.js
+++ index.js
@@ -2,15 +2,21 @@ var rect = require('./rectangle');
 
 function solveRect(l,b){
     console.log("solving for rectangle with legth = "+ l + " and breadth = "+ b);
-    if(l<= 0 || b<= 0){
-        console.log("invalid dimmentions");
-    }
-    else{
-        console.log(" perimeter = " + rect.perimeter(l,b) );
-        console.log("area = " + rect.area(l,b));
-    }
+    rect(l,b,(err,rectangle) => {
+        if(err){
+            console.log("ERROR : " + err.message);
+        }
+        else{
+            console.log("The area of the rectangle of dimensions l = "
+            + l + " and b = " + b + " is " + rectangle.area());
+            console.log("The perimeter of the rectangle of dimensions l = "
+            + l + " and b = " + b + " is " + rectangle.perimeter());
+        }
+    });
+    console.log("this message is actually after calling of rect");
+};
 
-}
+
 solveRect(2,4);
 solveRect(0,3);
 solveRect(-3,5);
--- rectangle.js
+++ rectangle.js
@@ -1,2 +1,20 @@
-exports.perimeter = (x,y) => (2*(x+y));
-exports.area = (x,y) => (x*y);
+module.exports = (a,b,callback) => {
+    if(a <= 0 || b <= 0){
+        setTimeout(() =>
+            callback(new Error("invalid input"),null)  
+        ,2000);
+    }
+    else{
+        setTimeout(() => 
+           callback(null,{
+            perimeter : () => (2*(a+b)),
+            area : () => (a*b)
+           }) 
+        ,2000);
+    }
+    
+}
+
+
+
+
